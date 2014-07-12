a390d2551a5e39ad35888c4b326f50212b60cabf
xsank <825128975@qq.com>
2014-07-12 17:33:31 +0800

    add description of exception

--- eventbus/exception.py
+++ eventbus/exception.py
@@ -4,6 +4,12 @@ __author__ = 'Xsank'
 class EventTypeError(Exception):
     '''Event type is invalid!'''
 
+    def __str__(self):
+        return self.__doc__
+
 
 class UnregisterError(Exception):
     '''No listener to unregister!'''
+
+    def __str__(self):
+        return self.__doc__
