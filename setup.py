3f32f9335f44e79588714fe887b85b153f4d17cf
xsank <825128975@qq.com>
2014-07-12 18:21:48 +0800

    add setup file

---
+++ setup.py
@@ -0,0 +1,13 @@
+__author__ = 'Xsank'
+
+from distutils.core import setup
+
+setup(
+    name='Pyeventbus',
+    version='0.0.1',
+    author='xsank',
+    email='xsank@foxmail.com',
+    license='MIT',
+    url='http://fastfood.sinaapp.com/',
+    packages=['eventbus']
+)
