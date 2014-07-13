fa51aeefe85feb010b2474f64011f5476b5d5280
xsank <825128975@qq.com>
2014-07-13 11:27:18 +0800

    improve the async event control

--- eventbus/eventbus.py
+++ eventbus/eventbus.py
@@ -1,16 +1,31 @@
 __author__ = 'Xsank'
 import inspect
 from multiprocessing.pool import ThreadPool
+from threading import Condition
+from threading import Thread
 
 from listener import Listener
 from exception import RegisterError
 from exception import UnregisterError
 
 
-class EventBus(object):
-    def __init__(self,pool_size=4):
+class EventBus(Thread):
+    def __init__(self,pool_size=4,is_daemon=True):
+        super(EventBus,self).__init__()
+        self.setDaemon(is_daemon)
         self.listeners=dict()
         self.pool=ThreadPool(pool_size)
+        self.events=list()
+        self.con=Condition()
+
+    def run(self):
+        while True:
+            self.con.acquire()
+            while not self.events:
+                self.con.wait()
+            self.pool.map(self.process,self.events)
+            self.events=list()
+            self.con.release()
 
     def register(self,listener):
         if not isinstance(listener,Listener):
@@ -23,13 +38,19 @@ class EventBus(object):
         except Exception:
             raise UnregisterError
 
-    def post(self,event):
+    def process(self,event):
         for listener in self.listeners.values():
             for name,func in inspect.getmembers(listener,predicate=inspect.ismethod):
                 func(event)
 
+    def post(self,event):
+        self.process(event)
+
     def async_post(self,event):
-        self.pool.map(self.post,(event,))
+        self.con.acquire()
+        self.events.append(event)
+        self.con.notifyAll()
+        self.con.release()
 
     def destroy(self):
         self.listeners.clear()
--- example/myeventbus.py
+++ example/myeventbus.py
@@ -1,4 +1,5 @@
 __author__ = 'Xsank'
+import time
 from eventbus.eventbus import EventBus
 
 from myevent import GreetEvent
@@ -9,8 +10,10 @@ from mylistener import MyListener
 if __name__=="__main__":
     eventbus=EventBus()
     eventbus.register(MyListener())
+    eventbus.start()
     ge=GreetEvent('world')
     be=ByeEvent('world')
     eventbus.post(ge)
     eventbus.async_post(be)
+    time.sleep(1)
     eventbus.unregister(MyListener())
