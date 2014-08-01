6a6f048e74a0564eb40fdc14d3132b08d86c2214
xsank <825128975@qq.com>
2014-08-01 13:57:09 +0800

    optimization of processing

--- eventbus/eventbus.py
+++ eventbus/eventbus.py
@@ -3,12 +3,14 @@ import inspect
 import thread
 from multiprocessing.pool import ThreadPool
 from threading import Condition
-from threading import Thread
 
+from event import Event
 from listener import Listener
 from util import Singleton
+from exception import EventTypeError
 from exception import RegisterError
 from exception import UnregisterError
+from exception import ProcessException
 
 
 class EventBus(object):
@@ -16,49 +18,64 @@ class EventBus(object):
 
     def __init__(self,pool_size=4):
         super(EventBus,self).__init__()
-        self.listeners=dict()
         self.pool=ThreadPool(pool_size)
-        self.events=list()
+        self.async_events=list()
+        self.event_handlers=dict()
         self.con=Condition()
         self.init()
 
     def init(self):
         thread.start_new_thread(self.loop,())
 
-    def loop(self):
-        while True:
-            self.con.acquire()
-            while not self.events:
-                self.con.wait()
-            self.pool.map(self.process,self.events)
-            self.events=list()
-            self.con.release()
-
     def register(self,listener):
         if not isinstance(listener,Listener):
             raise RegisterError
-        self.listeners[listener.__class__.__name__]=listener
+
+        for name,func in EventBus.get_listener_handlers(listener):
+            if func.event in self.event_handlers:
+                self.event_handlers[func.event].append(func)
+            else:
+                self.event_handlers[func.event]=[func]
 
     def unregister(self,listener):
         try:
-            self.listeners.pop(listener.__class__.__name__)
+            for name,func in EventBus.get_listener_handlers(listener):
+                del self.event_handlers[func.event]
         except Exception:
             raise UnregisterError
 
     def process(self,event):
-        for listener in self.listeners.values():
-            for name,func in inspect.getmembers(listener,predicate=inspect.ismethod):
-                func(event)
+        handlers=self.event_handlers[event.__class__.__name__]
+        if not handlers:
+            raise ProcessException
+
+        for handler in handlers:
+            handler(event)
 
     def post(self,event):
+        if not isinstance(event,Event):
+            raise EventTypeError
         self.process(event)
 
     def async_post(self,event):
         self.con.acquire()
-        self.events.append(event)
+        self.async_events.append(event)
         self.con.notifyAll()
         self.con.release()
 
+    def loop(self):
+        while True:
+            self.con.acquire()
+            while not self.async_events:
+                self.con.wait()
+            self.pool.map(self.process,self.async_events)
+            self.async_events=list()
+            self.con.release()
+
+    @staticmethod
+    def get_listener_handlers(listener):
+        return inspect.getmembers(listener,predicate=inspect.ismethod)
+
     def destroy(self):
-        self.listeners.clear()
+        self.event_handlers.clear()
         self.pool.close()
--- example/mylistener.py
+++ example/mylistener.py
@@ -1,17 +1,18 @@
 __author__ = 'Xsank'
 from eventbus.listener import Listener
-from eventbus.listener import check_event
+from eventbus.listener import add_event
 
 from myevent import GreetEvent
 from myevent import ByeEvent
 
 
 class MyListener(Listener):
-    @check_event(GreetEvent)
+    @add_event(GreetEvent)
     def greet(self,event=None):
         print 'hello',event.name
 
-    @check_event(ByeEvent)
+    @add_event(ByeEvent)
     def goodbye(self,event=None):
         print 'bye',event.name
+
 
