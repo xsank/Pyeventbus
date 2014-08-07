9700e142c209e50d4f5b128760d21f3aced16aef
xsank <825128975@qq.com>
2014-08-07 12:43:04 +0800

    modify the example

--- example/myevent.py
+++ example/myevent.py
@@ -4,11 +4,9 @@ from eventbus.event import Event
 
 class GreetEvent(Event):
     def __init__(self,name):
-        super(GreetEvent,self).__init__()
         self.name=name
 
 
 class ByeEvent(Event):
     def __init__(self,name):
-        super(ByeEvent,self).__init__()
         self.name=name
